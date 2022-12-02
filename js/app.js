6767a82e3c0c2dfebea83dce8e114987d98e2e48
Jordan Covington <Atlantafan1087@gmail.com>
2022-12-02 12:54:51 -0800

    Forgot to branch

--- js/app.js
+++ js/app.js
@@ -85,7 +85,6 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 function sumArray(sumArr) { //eslint-disable-line
 
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
